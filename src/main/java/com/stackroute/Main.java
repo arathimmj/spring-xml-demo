28feca5c3d84ebd5672c5cf47ec44e5d4f02965a
Arathi <mj.arathi@gmail.com>
2019-05-15 09:41:21 +0530

    Implemented aware interface

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -16,13 +16,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Movie movies;
 
-        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context=
+                new ClassPathXmlApplicationContext("beans.xml");
 
         System.out.println("Using autowired name");
         Movie movie = (Movie) context.getBean("movie1");
         System.out.println("Actor Name:\t"+movie.getActor().getActorName()+"\nActor Gender:\t"+movie.getActor().getActorGender()+"\nActor Age:\t"+movie.getActor().getActorAge()+"\n");
-
         System.out.println();
 
         System.out.println("Using autowired constructor");
@@ -30,10 +31,10 @@ public class Main {
         System.out.println("Actor Name:\t"+movie2.getActor().getActorName()+"\nActor Gender:\t"+movie2.getActor().getActorGender()+"\nActor Age:\t"+movie2.getActor().getActorAge()+"\n");
         System.out.println();
 
-        System.out.println("Using autowired type");
+        /*System.out.println("Using autowired type");
         Movie movie3 = (Movie) context.getBean("movie3");
         System.out.println("Actor Name:\t"+movie3.getActor().getActorName()+"\nActor Gender:\t"+movie3.getActor().getActorGender()+"\nActor Age:\t"+movie3.getActor().getActorAge()+"\n");
-
+*/
     }
 
 }
